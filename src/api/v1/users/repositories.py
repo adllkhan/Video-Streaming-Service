b416d916ad9c04e7410f1b3e5402003151a43dd0
adllkhan <aomertayevich@gmail.com>
2025-05-26 23:12:18 +0500

    Revert "added more exception classes"
    This reverts commit 8c229ee34db12fa53214c5c7a7143dd0e83d5100.

--- src/api/v1/users/repositories.py
+++ src/api/v1/users/repositories.py
@@ -1,12 +1,9 @@
-from sqlalchemy.exc import IntegrityError, SQLAlchemyError
+from sqlalchemy.exc import IntegrityError
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
 
-from src.exceptions import (
-    HTTPAlreadyExists,
-    HTTPNotFound,
-    HTTPDatabaseError,
-)
+from src.exceptions import HTTPAlreadyExists
+
 from .models import User
 
 
@@ -15,50 +12,32 @@ class UserRepository:
         self.session = session
 
     async def get_users(self) -> list[User]:
-        try:
-            stmt = select(User)
-            result = await self.session.execute(stmt)
-            return result.scalars().all()
-        except SQLAlchemyError as e:
-            raise HTTPDatabaseError(error=str(e))
+        stmt = select(User)
+        result = await self.session.execute(stmt)
+        return result.scalars().all()
 
     async def get_user(self, user_id: int) -> User:
-        try:
-            stmt = select(User).where(User.id == user_id)
-            result = await self.session.execute(stmt)
-            user = result.scalars().first()
-            if not user:
-                raise HTTPNotFound(model="User", search={"id": user_id})
-            return user
-        except SQLAlchemyError as e:
-            raise HTTPDatabaseError(error=str(e))
+        stmt = select(User).where(User.id == user_id)
+        result = await self.session.execute(stmt)
+        return result.scalars().first()
 
     async def create_user(self, user: User) -> User:
         self.session.add(user)
         try:
             await self.session.commit()
-            await self.session.refresh(user)
-            return user
         except IntegrityError:
-            raise HTTPAlreadyExists(model="User", request={"username": user.username})
-        except SQLAlchemyError as e:
-            raise HTTPDatabaseError(error=str(e))
+            raise HTTPAlreadyExists(model=User, request={"username": user.username})
+        await self.session.refresh(user)
+        return user
 
     async def update_user(self, user: User) -> User:
-        try:
-            await self.session.commit()
-            await self.session.refresh(user)
-            return user
-        except SQLAlchemyError as e:
-            raise HTTPDatabaseError(error=str(e))
+        await self.session.commit()
+        await self.session.refresh(user)
+        return user
 
     async def delete_user(self, user_id: int) -> User:
-        try:
-            user = await self.get_user(user_id=user_id)
+        user = await self.get_user(user_id=user_id)
+        if user:
             await self.session.delete(user)
             await self.session.commit()
-            return user
-        except HTTPNotFound:
-            raise
-        except SQLAlchemyError as e:
-            raise HTTPDatabaseError(error=str(e))
+        return user
